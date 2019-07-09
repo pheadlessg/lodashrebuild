f96f738dfa11c59270713caac04d2304710d12ca
pheadlessg <seankfisher@gmail.com>
2019-07-08 23:03:29 -0700

    Project set up, beginning on identity

---
+++ lodashrebuild.js
@@ -0,0 +1,11 @@
+const _ = {};
+
+_.identity = x => {};
+_.take = (collection, x) => {};
+_.uniq = collection => {};
+_.map = (collection, func) => {};
+_.forEach = (collection, func) => {};
+_.filter = (collection, func) => {};
+_.reduce = (collection, func, accumulator) => {};
+_.once = func => {};
+_.before = func => {};
---
+++ spec/lodashrebuild.spec.js
@@ -0,0 +1,4 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const _ = require('../lodashrebuild');
