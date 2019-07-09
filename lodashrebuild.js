d5225ac6d32f7bcda735ddf1da361e9e5d20b649
pheadlessg <seankfisher@gmail.com>
2019-07-08 23:13:48 -0700

    identity and tests finished, moving onto take

--- lodashrebuild.js
+++ lodashrebuild.js
@@ -1,6 +1,8 @@
 const _ = {};
 
-_.identity = x => {};
+_.identity = x => {
+  return x;
+};
 _.take = (collection, x) => {};
 _.uniq = collection => {};
 _.map = (collection, func) => {};
@@ -9,3 +11,5 @@ _.filter = (collection, func) => {};
 _.reduce = (collection, func, accumulator) => {};
 _.once = func => {};
 _.before = func => {};
+
+module.exports = _;
--- spec/lodashrebuild.spec.js
+++ spec/lodashrebuild.spec.js
@@ -2,3 +2,15 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const _ = require('../lodashrebuild');
+
+describe('#identity', () => {
+  it('returns the value passed as an argument', () => {
+    expect(_.identity(3)).to.equal(3);
+    expect(_.identity('hello')).to.equal('hello');
+    expect(_.identity(true)).to.equal(true);
+  });
+  it('returns a reference to the same reference type passed as an argument', () => {
+    const arr = [];
+    expect(_.identity(arr)).to.equal(arr);
+  });
+});
